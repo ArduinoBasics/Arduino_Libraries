573cf2861dad3373bb4c73569e1a2937703187da
Scott C <ArduinoBasics@users.noreply.github.com>
2019-12-29 16:32:38 +0800

    Create blinkTest.ino

---
+++ BlinkMe/examples/blinkTest/blinkTest.ino
@@ -0,0 +1,21 @@
+/*
+Filename: 		blinkTest.ino
+Example sketch: Testing the BlinkMe library
+Author: 		Scott C / ArduinoBasics
+Date:			28th Nov 2019
+Microcontroller: Arduino UNO
+IDE Version: 1.8.5 (Windows 10)
+https://github.com/ArduinoBasics/Arduino_Libraries/tree/master/BlinkMe
+*/
+
+#include <BlinkMe.h>
+BlinkMe bM;
+
+void setup(){
+	//bM.setOUTPUT(13);        //Use this function to blink on a different pin (default is Pin13)
+}
+
+void loop(){
+	bM.blink(1000);           //Blink ON for 1 second, OFF for 1 second (1000 millis)	
+	bM.blink(2000);			  //Blink ON for 2 seconds, OFF for 2 seconds
+}
